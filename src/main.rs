fc4ec0efd5dd43386b581989d8910b4f02bc9b99
Tony Solomonik <tony.solomonik@gmail.com>
2023-03-03 20:22:49 +0200

    Write 10000 integers at random order in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 use dbil::lsm_tree::LSMTree;
 use glommio::{LocalExecutorBuilder, Placement};
+use rand::{seq::SliceRandom, thread_rng};
 use std::time::Duration;
 use std::{env::temp_dir, io::Result, path::PathBuf};
 
-macro_rules! hashmap {
-    ($( $key: expr => $val: expr ),*) => {{
-         let mut map = ::std::collections::HashMap::new();
-         $( map.insert($key, $val); )*
-         map
-    }}
-}
-
-async fn run(dir: PathBuf) -> Result<()> {
+async fn write(dir: PathBuf) -> Result<()> {
     let mut tree = LSMTree::new(dir).await?;
 
-    let entries = hashmap![
-        "B".to_string() => "are".to_string(),
-        "A".to_string() => "Trees".to_string(),
-        "C".to_string() => "cool".to_string()
-    ];
-    for (key, value) in entries {
-        tree.set(key, value).await?;
+    let mut rng = thread_rng();
+    let mut nums: Vec<u32> = (0..10000).collect();
+    nums.shuffle(&mut rng);
+    for i in nums {
+        tree.set(i.to_string(), i.to_string()).await?;
     }
 
-    let lookup_key = "C".to_string();
+    let lookup_key = "5000".to_string();
+    println!("Querying for key '{}'", lookup_key);
+
+    if let Ok(Some(v)) = tree.get(&lookup_key).await {
+        println!("Found: {}", v);
+    } else {
+        println!("Key not found");
+    }
+
+    Ok(())
+}
+
+async fn read(dir: PathBuf) -> Result<()> {
+    let tree = LSMTree::new(dir).await?;
+
+    let lookup_key = "1234".to_string();
     println!("Querying for key '{}'", lookup_key);
 
     if let Ok(Some(v)) = tree.get(&lookup_key).await {
@@ -41,7 +47,8 @@ fn main() -> Result<()> {
     let handle = builder.name("dbil").spawn(|| async move {
         let mut db_dir = temp_dir();
         db_dir.push("dbil");
-        run(db_dir).await
+        write(db_dir.clone()).await?;
+        read(db_dir).await
     })?;
 
     handle.join()??;
