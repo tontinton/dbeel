caa628d3f0b444abda365b4747e91be19d576d44
Tony Solomonik <tony.solomonik@gmail.com>
2023-09-22 15:05:21 +0300

    storage-engine: cached-file-reader: Add read_at_into

--- src/storage_engine/cached_file_reader.rs
+++ src/storage_engine/cached_file_reader.rs
@@ -22,8 +22,12 @@ impl CachedFileReader {
         Self { id, file, cache }
     }
 
-    pub async fn read_at(&self, pos: u64, size: usize) -> Result<Vec<u8>> {
-        assert_ne!(size, 0);
+    pub async fn read_at_into(
+        &self,
+        pos: u64,
+        output_buf: &mut [u8],
+    ) -> Result<()> {
+        let size = output_buf.len();
 
         // The address to the first page.
         let low_page = align_down(pos);
@@ -31,7 +35,7 @@ impl CachedFileReader {
         // The address to the last page, we don't read it.
         let high_page = align_up(pos + size as u64);
 
-        let mut output_buf = Vec::with_capacity(size);
+        let mut written = 0;
 
         for address in (low_page..high_page).step_by(PAGE_SIZE) {
             let start = if address == low_page {
@@ -39,17 +43,22 @@ impl CachedFileReader {
             } else {
                 0
             };
-            let end = std::cmp::min(PAGE_SIZE, size - output_buf.len() + start);
+            let end = std::cmp::min(PAGE_SIZE, size - written + start);
+            let write_size = end - start;
 
             match self.cache.get(self.id, address) {
                 Some(page) => {
-                    output_buf.extend_from_slice(&page[start..end]);
+                    output_buf[written..written + write_size]
+                        .copy_from_slice(&page[start..end]);
+                    written += write_size;
                 }
                 None => {
                     let page =
                         self.file.read_at_aligned(address, PAGE_SIZE).await?;
 
-                    output_buf.extend_from_slice(&page[start..end]);
+                    output_buf[written..written + write_size]
+                        .copy_from_slice(&page[start..end]);
+                    written += write_size;
 
                     let mut page_buf = [0; PAGE_SIZE];
                     page_buf[..page.len()].copy_from_slice(&page);
@@ -59,7 +68,15 @@ impl CachedFileReader {
             }
         }
 
-        assert_eq!(output_buf.len(), size);
+        assert_eq!(written, size);
+        Ok(())
+    }
+
+    pub async fn read_at(&self, pos: u64, size: usize) -> Result<Vec<u8>> {
+        assert_ne!(size, 0);
+
+        let mut output_buf = vec![0; size];
+        self.read_at_into(pos, &mut output_buf).await?;
 
         Ok(output_buf)
     }
