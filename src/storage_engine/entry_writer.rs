cc586bfb54179cc8a33998c255db0d4f1047d856
Tony Solomonik <tony.solomonik@gmail.com>
2023-10-13 18:54:59 +0300

    storage-engine: entry-writer: Add write behind buffers to index writing
    By using my fork, where I have added the `truncate_on_close` feature
    to `DmaStreamWriter`.

--- src/storage_engine/entry_writer.rs
+++ src/storage_engine/entry_writer.rs
@@ -38,8 +38,9 @@ impl EntryWriter {
         );
         let index_writer = Box::new(
             DmaStreamWriterBuilder::new(index_file)
-                .with_write_behind(1)
+                .with_write_behind(DMA_STREAM_NUMBER_OF_BUFFERS)
                 .with_buffer_size(INDEX_ENTRY_SIZE)
+                .with_truncate_on_close(true)
                 .build(),
         );
 
